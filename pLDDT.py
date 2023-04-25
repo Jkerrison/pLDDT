aaa10c298fce32332926b021a8ff91841e74ffe9
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-25 15:29:51 +0100

    Update pLDDT.py

--- pLDDT.py
+++ pLDDT.py
@@ -3,10 +3,10 @@ from pymol import cmd
 
 def pLDDT(selection="all"):
 
-    cmd.color("0x0053D6", f"({selection}) and b > 90")
-    cmd.color("0x65CBF3", f"({selection}) and b < 90 and b > 70")
-    cmd.color("0xFFDB13", f"({selection}) and b < 70 and b > 50")
-    cmd.color("0xFF7D45", f"({selection}) and b < 50")
+    cmd.color("0x0053D6", f"({selection}) and b > 0.90")
+    cmd.color("0x65CBF3", f"({selection}) and b < 0.90 and b > 0.70")
+    cmd.color("0xFFDB13", f"({selection}) and b < 0.70 and b > 0.50")
+    cmd.color("0xFF7D45", f"({selection}) and b < 0.50")
 
 
 cmd.extend("pLDDT", pLDDT)
