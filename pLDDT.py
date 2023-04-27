fa46642063b7953840530e95abcfdb72db312773
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-27 11:57:00 +0100

    Update pLDDT.py

--- pLDDT.py
+++ pLDDT.py
@@ -15,14 +15,18 @@ cmd.extend("esm_pLDDT", esm_pLDDT)
 cmd.auto_arg[0]["esm_pLDDT"] = [cmd.object_sc, "object", ""]
 
     
-def af_pLDDT(selection="all"):
+def pLDDT(selection="atom"):
 
     cmd.color("0x0053D6", f"({selection}) and b > 90")
     cmd.color("0x65CBF3", f"({selection}) and b < 90 and b > 70")
     cmd.color("0xFFDB13", f"({selection}) and b < 70 and b > 50")
     cmd.color("0xFF7D45", f"({selection}) and b < 50")
+    cmd.color("0x0053D6", f"({selection}) and b < 1 and b > 0.89")
+    cmd.color("0x65CBF3", f"({selection}) and b < 0.9 and b > 0.69")
+    cmd.color("0xFFDB13", f"({selection}) and b < 0.7 and b > 0.49")
+    cmd.color("0xFF7D45", f"({selection}) and b < 0.5")
     
 #
 
-cmd.extend("af_pLDDT", af_pLDDT)
-cmd.auto_arg[0]["af_pLDDT"] = [cmd.object_sc, "object", ""]
+cmd.extend("pLDDT", pLDDT)
+cmd.auto_arg[0]["pLDDT"] = [cmd.object_sc, "object", ""]
