d1b91f116dafadd02bba358ee47dda77986a4950
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-27 10:13:02 +0100

    Update pLDDT.py

--- pLDDT.py
+++ pLDDT.py
@@ -1,7 +1,7 @@
 from pymol import cmd
 
 
-def pLDDT(selection="all"):
+def pLDDT(selection="atoms"):
 
     cmd.color("0x0053D6", f"({selection}) and b => 0.9")
     cmd.color("0x65CBF3", f"({selection}) and b < 0.9 and b => 0.7")
