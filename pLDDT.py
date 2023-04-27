6cf591cd9e08121bfe73b974cbb9b929f9fbdda4
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-27 12:00:12 +0100

    Update pLDDT.py

--- pLDDT.py
+++ pLDDT.py
@@ -17,7 +17,7 @@ cmd.extend("esm_pLDDT", esm_pLDDT)
     
 def pLDDT(selection="atom"):
 
-    cmd.color("0x0053D6", f"({selection}) and b > 90 or b < 1 and b > 0.89")
+    cmd.color("red", f"({selection}) and b > 90 or b < 1 and b > 0.89")
     cmd.color("0x65CBF3", f"({selection}) and b < 90 and b > 70")
     cmd.color("0xFFDB13", f"({selection}) and b < 70 and b > 50")
     cmd.color("0xFF7D45", f"({selection}) and b < 50")
