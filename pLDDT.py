1ef6dfce6ec01abd975efd8df801d4cc9c04846c
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-27 10:20:50 +0100

    Update pLDDT.py

--- pLDDT.py
+++ pLDDT.py
@@ -1,7 +1,7 @@
 from pymol import cmd
 
 
-def pLDDT(selection="all"):
+def esmpLDDT(selection="all"):
 
     cmd.color("0x0053D6", f"({selection}) and b > 0.9")
     #cmd.color("red", f"({selection}) and b = 0.9")
@@ -9,7 +9,19 @@ def pLDDT(selection="all"):
     cmd.color("0xFFDB13", f"({selection}) and b < 0.7 and b > 0.5")
     cmd.color("0xFF7D45", f"({selection}) and b < 0.5")
     
+
+cmd.extend("esmpLDDT", esmpLDDT)
+cmd.auto_arg[0]["esmpLDDT"] = [cmd.object_sc, "object", ""]
+    
+def afpLDDT(selection="all"):
+
+    cmd.color("0x0053D6", f"({selection}) and b > 90")
+    #cmd.color("red", f"({selection}) and b = 0.9")
+    cmd.color("0x65CBF3", f"({selection}) and b < 90 and b > 70")
+    cmd.color("0xFFDB13", f"({selection}) and b < 70 and b > 50")
+    cmd.color("0xFF7D45", f"({selection}) and b < 50")
+    
 #
 
-cmd.extend("pLDDT", pLDDT)
-cmd.auto_arg[0]["pLDDT"] = [cmd.object_sc, "object", ""]
+cmd.extend("afpLDDT", afpLDDT)
+cmd.auto_arg[0]["afpLDDT"] = [cmd.object_sc, "object", ""]
