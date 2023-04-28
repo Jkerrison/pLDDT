bcec91b6a1631921e35f6e902f3f1c38a7b73f9b
Jkerrison <118831033+Jkerrison@users.noreply.github.com>
2023-04-28 02:24:46 +0100

    Update pLDDT.py

--- pLDDT.py
+++ pLDDT.py
@@ -1,18 +1,18 @@
 from pymol import cmd
 
 
-def esm_pLDDT(selection="atom"):
+# def esm_pLDDT(selection="atom"):
 
-    cmd.color("0x0053D6", f"({selection}) and b > 0.89")
-    cmd.color("0x65CBF3", f"({selection}) and b < 0.9 and b > 0.69")
-    cmd.color("0xFFDB13", f"({selection}) and b < 0.7 and b > 0.49")
-    cmd.color("0xFF7D45", f"({selection}) and b < 0.5")
-    #Appears to throw an error if <= is used so thats why there is a slight overlap in quality, rounds up
-    #ESMfold predicts per atom pLDDT between 0-1 whereas Alphafold predics per residue values between 0-100
+#     cmd.color("0x0053D6", f"({selection}) and b > 0.89")
+#     cmd.color("0x65CBF3", f"({selection}) and b < 0.9 and b > 0.69")
+#     cmd.color("0xFFDB13", f"({selection}) and b < 0.7 and b > 0.49")
+#     cmd.color("0xFF7D45", f"({selection}) and b < 0.5")
+#     #Appears to throw an error if <= is used so thats why there is a slight overlap in quality, rounds up
+#     #ESMfold predicts per atom pLDDT between 0-1 whereas Alphafold predics per residue values between 0-100
     
 
-cmd.extend("esm_pLDDT", esm_pLDDT)
-#cmd.auto_arg[0]["esm_pLDDT"] = [cmd.object_sc, "object", ""]
+# cmd.extend("esm_pLDDT", esm_pLDDT)
+# #cmd.auto_arg[0]["esm_pLDDT"] = [cmd.object_sc, "object", ""]
 
     
 def pLDDT(selection="atom"):
